39456b09a319d1115e5109edfa2e10dce4c15278
Elsayed Mohamed <elsayedmo2125@gmail.com>
2026-08-03 23:33:32 +0300

    create masssege with grade

--- student.py
+++ student.py
@@ -4,11 +4,16 @@ print("Hello, " + name + "! Welcome to the student portal.")
 degree=(int(input("Enter your degree program: ")))
 if degree>=90:
     print("A")
+    print("Congratulations! You have achieved an excellent grade.")
 elif degree>=80:
     print("B")
+    print("Good job! You have achieved a good grade.")
 elif degree>=70:
     print("C")
+    print("You have achieved a satisfactory grade.")
 elif degree>=60:
-    print("D")  
+    print("D")
+    print("You have achieved a passing grade.")
 else:
-    print("F")    
+    print("F")
+    print("Unfortunately, you have failed the course.")
