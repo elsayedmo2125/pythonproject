6ae5f6b215020ca2e3ebf801c7fbb3bd6b8584da
Elsayed Mohamed <elsayedmo2125@gmail.com>
2026-08-03 23:18:39 +0300

    Add grade calc

--- student.py
+++ student.py
@@ -1,2 +1,14 @@
 name=input("Enter your name: ")
 print("Hello, " + name + "! Welcome to the student portal.")    
+
+degree=(int(input("Enter your degree program: ")))
+if degree>=90:
+    print("A")
+elif degree>=80:
+    print("B")
+elif degree>=70:
+    print("C")
+elif degree>=60:
+    print("D")  
+else:
+    print("F")    
